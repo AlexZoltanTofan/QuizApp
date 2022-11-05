64a52e178109541472d45565a8f7f0a4a203ab7a
AlexZoltanTofan <alextofan99@.yahoo.com>
2022-11-05 18:35:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let questions = [
+  {
+    question: 'Wer hat HTML efunden?',
+    answer_1: 'Robbie Williams',
+    answer_2: 'Lady Gaga',
+    answer_3: 'Tim Berners-Lee',
+    answer_4: 'Justin Bieber',
+    right_answer: 3,
+  },
+
+  {
+    question: 'Was ist keine gültige Schreibweise für die Farbe Weiß?',
+    answer_1: 'rgba(255,255,255,1)',
+    answer_2: 'white',
+    answer_3: '#ff',
+    answer_4: '#FFF',
+    right_answer: 3,
+  },
+  {
+    question:
+      'top positioniert ein HTML-Element von oben. Mit welchem Wert für position funktioniert top nicht?',
+    answer_1: 'fixed',
+    answer_2: 'absolute',
+    answer_3: 'relative',
+    answer_4: 'static',
+    right_answer: 4,
+  },
+  {
+    question: 'Was ist nicht das gleiche wie margin: 10px?',
+    answer_1: 'margin: 10px 10px 10px 10px ',
+    answer_2: 'margin: 10px 10px',
+    answer_3: 'margin: 10px 10px 10px',
+    answer_4: 'margin: 10 ',
+    right_answer: 4,
+  },
+  {
+    question: 'Welchen Strukturselektor gibt es breits seit CSS 1?',
+    answer_1: ':first-letter ',
+    answer_2: ':root',
+    answer_3: ':last-child',
+    answer_4: ':first-child',
+    right_answer: 1,
+  },
+
+  {
+    question:
+      'Die Eigenschaft display hat seit CSS 3 neue Werte. Welcher ist hinzugekommen?',
+    answer_1: 'table-cell',
+    answer_2: 'grid-row',
+    answer_3: 'inline-flex',
+    answer_4: 'columns',
+    right_answer: 3,
+  },
+  {
+    question: 'Wie heißt der Erfinder von JavaScript?',
+    answer_1: 'Douglas Crockford',
+    answer_2: 'Brendan Eich',
+    answer_3: 'John Resig',
+    answer_4: 'Chuck Norris',
+    right_answer: 2,
+  },
+  {
+    question: 'Wer passt hier nicht in die Reihe?',
+    answer_1: 'ActionScript',
+    answer_2: 'JavaScript',
+    answer_3: 'CoffeeScript',
+    answer_4: 'AppleScript',
+    right_answer: 4,
+  },
+  {
+    question: 'Was ist false?',
+    answer_1: '1/0 === Infinity',
+    answer_2: '1.8e+987 === Infinity',
+    answer_3: 'Number.MIN_VALUE * -2 === -Infinity',
+    answer_4: 'Number.MAX_VALUE * 2 === Infinity',
+    right_answer: 3,
+  },
+  {
+    question: 'Was ist true?',
+    answer_1: '0 == null',
+    answer_2: '0 >= undefined',
+    answer_3: '0 >= null',
+    answer_4: '0 > null',
+    right_answer: 3,
+  },
+];
